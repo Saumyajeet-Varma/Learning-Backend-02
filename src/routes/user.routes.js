f35763eb47c8cfec36feed400c30afab4a6f5be6
Saumyajeet-Varma <saumyajeetv@gmail.com>
2024-12-21 17:13:33 +0530

    Add new user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js"
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import verifyJwt from "../middlewares/auth.middleware.js";
 
@@ -9,11 +9,11 @@ const userRouter = Router();
 userRouter.route("/register").post(
     upload.fields([
         {
-            name: "avatar", // This should be same in Frontend (name of the input)
+            name: "avatar", // ! This should be same in Frontend (name of the input)
             maxCount: 1,
         },
         {
-            name: "coverImage", // This should be same in Frontend (name of the input)
+            name: "coverImage", // ! This should be same in Frontend (name of the input)
             maxCount: 1,
         }
     ]),
@@ -30,5 +30,26 @@ userRouter.route("/logout").post(verifyJwt, logoutUser)
 
 // http://localhost:8000/api/v1/user/refresh-token
 userRouter.route("/refresh-token").post(refreshAccessToken)
+
+// http://localhost:8000/api/v1/user/change-password
+userRouter.route("/change-password").post(verifyJwt, changePassword)
+
+// http://localhost:8000/api/v1/user/current-user
+userRouter.route("/current-user").get(verifyJwt, getCurrentUser)
+
+// http://localhost:8000/api/v1/user/update-account
+userRouter.route("/update-account").patch(updateAccountDetails)
+
+// http://localhost:8000/api/v1/user/avatar
+userRouter.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
+
+// http://localhost:8000/api/v1/user/cover-image
+userRouter.route("/cover-image").patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage)
+
+// http://localhost:8000/api/v1/user/c/:username  // ! ':' is imp (req.params)
+userRouter.route("/c/:username").get(verifyJwt, getUserChannelProfile)
+
+// http://localhost:8000/api/v1/user/history
+userRouter.route("/history").get(verifyJwt, getWatchHistory)
 
 export default userRouter
